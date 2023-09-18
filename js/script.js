fd662264a4f3142f5019595ed50c2a9ac7da1982
Valentina Varela Rodríguez <vavarela.rodriguez@gmail.com>
2023-09-18 20:57:14 -0300

    wip: se agrega el zip entregado para la 1ra entrega y form para hacer pedido

---
+++ js/script.js
@@ -0,0 +1,20 @@
+function agregarAlPedido() {
+  // TODO: al hacer click en agregar al pedido disparar un confirm preguntando si desea agregar otro plato
+  //      Si elige que sí:
+  //          - Agregar en el div con id 'platos' otro label con su respectivo select para seleccionar el plato
+  //          - Agregar  en el div con id 'platos' en pedidoForm otro label con su respectivo input para seleccionar la cantidad
+  //
+  //      Si elige que no, enviar:
+  //          - Llamar a la funcion enviar()
+  // https://www.w3schools.com/js/js_popup.asp
+}
+
+function enviar() {
+  // TODO:
+  //      Si el usuario elige enviar:
+  //          - Parsear cantidad a numero y verificar cantidad > 0 (parseInt())
+  //          - El select debe tener una opcion seleccionada (elemento.selectedIndex > 0)
+  //      Si las validaciones estan ok:
+  //          - Mostrar un alert indicando que el pedido esta en preparacion
+  //          - Resetear campos
+}
